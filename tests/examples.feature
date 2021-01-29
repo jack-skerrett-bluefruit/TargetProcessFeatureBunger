80cbe636420e924a5289d133cc6e0660f2936944
jack-skerrett-bluefruit <37108313+jack-skerrett-bluefruit@users.noreply.github.com>
2021-01-29 12:10:15 +0000

    added in tests for checking example tables are read in correctly

---
+++ tests/examples.feature
@@ -0,0 +1,21 @@
+Scenario: This is a test title
+Given a set up
+When an <action> occurs
+Then there is an outcome
+
+Examples:
+|action|
+|earthquake|
+|realisation|
+
+Scenario: This is another test title
+Given a <amount> different set up
+When an similar <action> occurs
+Then there is a different outcome
+
+Examples:
+|amount|action|
+|slightly|earthquake|
+|slightly|realisation|
+|hugely|earthquake|
+|hugely|realisation|
--- tests/test_feature_file_reader.py
+++ tests/test_feature_file_reader.py
@@ -1,5 +1,5 @@
+from pytest import fixture
 from feature_file_reader import Reader
-from pytest import mark, fixture
 
 @fixture
 def test_reader():
@@ -8,6 +8,10 @@ def test_reader():
 @fixture
 def tests_reader():
     return Reader("tests/tests.feature")
+
+@fixture
+def examples_reader():
+    return Reader("tests/examples.feature")
 
 def test_reader_initialises_with_given_feature_file(test_reader):
     assert test_reader.file_name == "tests/test.feature"
@@ -27,3 +31,19 @@ def test_multiple_tests_are_stored_in_a_list_separated_by_a_line(tests_reader):
     with open("tests/tests.feature") as f:
         tests = f.read().splitlines()
     assert tests_reader.feature_file == tests
+
+def test_that_there_is_no_empty_line_between_the_last_step_of_a_test_and_an_examples_table(examples_reader):
+    examples_reader.set_feature_file()
+    i = -1 
+    for line in examples_reader.feature_file:
+        if(line == "Examples:"):
+            assert examples_reader.feature_file[i] is not ""
+        i += 1
+
+def test_that_there_is_an_empty_line_only_between_scenarios(tests_reader):
+    tests_reader.set_feature_file()
+    i = 1
+    for line in tests_reader.feature_file:
+        if(line == ""):
+            assert tests_reader.feature_file[i][:8] == "Scenario"
+        i += 1
