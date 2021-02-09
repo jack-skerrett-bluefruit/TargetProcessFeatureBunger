bea6784dded15c8e66fab9fa3682b68112a9308e
Jack Skerrett <jack.skerrett@bluefruit.co.uk>
2021-02-09 22:41:04 +0000

    Improved reading to take in tests as a list, rather than a whole test as a whole list item

---
+++ tests/examples_no_spaces.feature
@@ -0,0 +1,10 @@
+Scenario: This is another test title
+Given a <amount> different set up
+When an similar <action> occurs
+Then there is a different outcome
+Examples:
+|amount|action|
+|slightly|earthquake|
+|slightly|realisation|
+|hugely|earthquake|
+|hugely|realisation|
--- tests/test_feature_file_reader.py
+++ tests/test_feature_file_reader.py
@@ -13,37 +13,53 @@ def tests_reader():
 def examples_reader():
     return Reader("tests/examples.feature")
 
+@fixture
+def examples_no_spaces_reader():
+    return Reader("tests/examples_no_spaces.feature")
+
 def test_reader_initialises_with_given_feature_file(test_reader):
     assert test_reader.file_name == "tests/test.feature"
 
 def test_reader_stores_test_as_a_list(test_reader):
-    test_reader.set_feature_file()
     assert type(test_reader.feature_file) is list
 
-def test_reader_stores_each_line_of_a_test_as_a_separate_list_item(test_reader):
-    test_reader.set_feature_file()
-    with open("tests/test.feature") as f:
-        test = f.read().splitlines()
-    assert test_reader.feature_file == test
-
-def test_multiple_tests_are_stored_in_a_list_separated_by_a_line(tests_reader):
-    tests_reader.set_feature_file()
-    with open("tests/tests.feature") as f:
-        tests = f.read().splitlines()
-    assert tests_reader.feature_file == tests
-
 def test_that_there_is_no_empty_line_between_the_last_step_of_a_test_and_an_examples_table(examples_reader):
-    examples_reader.set_feature_file()
     i = -1 
     for line in examples_reader.feature_file:
         if(line == "Examples:"):
             assert examples_reader.feature_file[i] is not ""
         i += 1
 
-def test_that_there_is_an_empty_line_only_between_scenarios(tests_reader):
-    tests_reader.set_feature_file()
-    i = 1
-    for line in tests_reader.feature_file:
-        if(line == ""):
-            assert tests_reader.feature_file[i][:8] == "Scenario"
-        i += 1
+def test_scenarios_are_stored_as_separate_list_items(tests_reader):
+    expected_feature_list = [
+        [
+            "Scenario: This is a test title",
+            "Given a set up",
+            "When an action occurs",
+            "Then there is an outcome"
+        ],
+        [
+            "Scenario: This is another test title",
+            "Given a slightly different set up",
+            "When a similar action occurs",
+            "Then there is a different outcome"
+        ]
+    ]
+    assert expected_feature_list == tests_reader.feature_file
+
+def test_a_feature_file_with_no_spaces_is_read_in_correctly(examples_no_spaces_reader):
+    expected_feature_list = [
+        [
+            "Scenario: This is another test title",
+            "Given a <amount> different set up",
+            "When an similar <action> occurs",
+            "Then there is a different outcome",
+            "Examples:",
+            "|amount|action|",
+            "|slightly|earthquake|",
+            "|slightly|realisation|",
+            "|hugely|earthquake|",
+            "|hugely|realisation|"
+        ]
+    ]
+    assert expected_feature_list == examples_no_spaces_reader.feature_file
