1359a92056419fc0ad06001dff046ec663bed3e1
Jack Skerrett <jack.skerrett@bluefruit.co.uk>
2021-02-09 22:42:04 +0000

    Jsonifier takes a read in feature file and compiles it to upload, currently working under the assumption something is being created

--- settings.py
+++ settings.py
@@ -2,9 +2,10 @@ from collections import defaultdict
 
 #target process dictionary object
 
-TP_DICT = {"Name": "", "TestSteps": {"Items": []}}
+TP_DICT = {"Name": "","Project":{"ID": ""}, "TestSteps": {"Items": []}}
 
 DEFAULT_TEST_STEP = {"ResourceType":"TestStep","Description":""}
 
 #TP
 TP_URL = "https://bluefruit.tpondemand.com/api/v1/"
+ACCESS_TOKEN = "testaccesstoken"
