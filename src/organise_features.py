add8a9b377da0d34e212e09328369f873292d16f
Jack Skerrett <jack.skerrett@bluefruit.co.uk>
2021-02-11 21:01:18 +0000

    link an existing test case to a known feature file

--- src/organise_features.py
+++ src/organise_features.py
@@ -1,5 +1,6 @@
 from settings import ID_DICT_LIST_ITEM, ASSIGN_TO_TEST_PLANS, TP_URL, ACCESS_TOKEN
 from copy import deepcopy
+import requests
 
 class Organiser():
     def __init__(self, feature, test_case):
@@ -17,5 +18,9 @@ class Organiser():
 
     def set_request_url(self):
         return TP_URL + "TestCases/" + str(self.test_case) + "?format=json&access_token=" + ACCESS_TOKEN
+
+    def link_test_case_to_feature(self):
+        response = requests.post(self.request_url, data = self.data)
+        return response.status_code, response.json()["Id"]
         
  
