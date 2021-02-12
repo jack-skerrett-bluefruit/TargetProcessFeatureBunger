6d048d67955ebdab74f2689295541b6cee2651e8
jack-skerrett-bluefruit <37108313+jack-skerrett-bluefruit@users.noreply.github.com>
2021-02-12 14:57:06 +0000

    more wip

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -15,10 +15,11 @@ def test_uploader_initialises_with_request_url_and_params_for_uploading_test_cas
 def test_uploader_uploads_single_test_file(mock_request_post, json_test_feature):
     mock_request_post.return_value = mock.Mock(name = "mock_api_response",
                                                 **{"status_code": 200, "json.return_value": '{"Items": [{"ResourceType":"TestCase","Id":12345}]}'})
-    test_case_uploader = Uploader("TestCase", json_test_feature.tp_format_feature_file)
+    test_case_uploader = Uploader("TestCases", json_test_feature.tp_format_feature_file)
     assert test_case_uploader.upload_new_test_cases() == (200, 12345)
 
 @mock.patch("src.upload.requests.post")
 def test_uploader_creates_a_single_feature_entity(mock_request_post):
     mock_request_post.return_value = mock.Mock(name = "mock_api_response",
                                                 **{"status_code": 200, "json.return_value": '{"Items": [{"ResourceType": "Feature","Id":87596}]'})
+    test_case_uploade= Uploader("Features", )
