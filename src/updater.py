6add8434637e830edabd24a281a63de5957df65b
jack-skerrett-bluefruit <37108313+jack-skerrett-bluefruit@users.noreply.github.com>
2021-02-25 17:01:05 +0000

    WIP requesting whether a feature exists

--- src/updater.py
+++ src/updater.py
@@ -1,9 +1,16 @@
 from settings import TP_URL, ACCESS_TOKEN
+import requests
 
 class Updater:
-    def __init__(self, feature_id):
+    def __init__(self, feature_name, feature_id, project_id, ):
+        self.feature_name = feature_name
         self.feature_id = feature_id
-        self.check_feature_exists_request = self.set_request_url()
+        self.project_id = project_id
+        self.check_feature_exists_request_url = self.set_request_url()
 
     def set_request_url(self):
-        return f"{TP_URL}Features/{self.feature_id}?include=[Id]&format=json&access_token={ACCESS_TOKEN}"
+        return f"{TP_URL}Features?where=(Name eq \'{self.feature_name}\') and (Project.Id eq {self.project_id})&include=[Id]&format=json&access_token={ACCESS_TOKEN}"
+
+    def check_feature(self):
+        response = requests.get(self.check_feature_exists_request_url)
+        return response.status_code
--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,13 +1,37 @@
 from pytest import fixture
 from src.updater import Updater
+import requests
 
+class FeatureCheckSuccessResponse:
+    status_code = 200
+
+class FeatureCheckFailureResponse:
+    status_code = 
 
 @fixture
 def feature_updater():
-    return Updater(99999)
+    return Updater("Feature: Whole Feature", 99999, 12345)
+
 
 def test_that_updater_intialises_with_feature_id(feature_updater):
     assert feature_updater.feature_id == 99999
 
+
 def test_updater_creates_a_request_to_check_that_a_feature_exists(feature_updater):
-    assert feature_updater.check_feature_exists_request == "https://bluefruit.tpondemand.com/api/v1/Features/99999?include=[Id]&format=json&access_token=testaccesstoken"
+    assert feature_updater.check_feature_exists_request_url == "https://bluefruit.tpondemand.com/api/v1/Features?where=(Name eq 'Feature: Whole Feature') and (Project.Id eq 12345)&include=[Id]&format=json&access_token=testaccesstoken"
+
+
+def test_updater_finds_a_feature(feature_updater, monkeypatch):
+    def mock_get(*args, **kwargs):
+        return FeatureCheckSuccessResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+
+    assert(feature_updater.check_feature() == 200)
+    
+
+
+def test_updater_does_not_find_a_feature():
+    def mock_get(*args, **kwargs):
+        return FeatureCheckFailureResponse()
+
