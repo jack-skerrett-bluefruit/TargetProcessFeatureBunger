77119ee48a8f142970716ff281083573bf5e2135
jack-skerrett-bluefruit <37108313+jack-skerrett-bluefruit@users.noreply.github.com>
2021-03-05 17:45:33 +0000

    updater can now get and set the test cases that are known to target process so that I can make sure only those are updated

--- src/updater.py
+++ src/updater.py
@@ -2,9 +2,8 @@ from settings import TP_URL, ACCESS_TOKEN
 import requests
 
 class Updater:
-    def __init__(self, feature_name, feature_id, project_id, ):
+    def __init__(self, feature_name, project_id, feature_file):
         self.feature_name = feature_name
-        self.feature_id = feature_id
         self.project_id = project_id
         self.check_feature_exists_request_url = self.set_request_url()
 
@@ -14,6 +13,18 @@ class Updater:
     def check_feature(self):
         response = requests.get(self.check_feature_exists_request_url)
         if(response.status_code == 200):
+            feature = response.json()
+            self.feature_id = feature["Items"][0]["Id"]
             return True
         else:   #I'm unsure about this. I want to look for a 404 to indicate an error, other status codes could mean there is an issue with the API, and the feature DOES exist
             return False
+
+    def set_test_cases_of_an_existing_feature(self):
+        existing_feature = self.get_test_cases_of_an_existing_feature()
+        self.test_cases = []
+        for test_case in existing_feature["LinkedTestPlan"]["TestCases"]["Items"]:
+            self.test_cases.append(test_case["Id"])
+
+    def get_test_cases_of_an_existing_feature(self):
+        response = requests.get(f"{TP_URL}Features/{self.feature_id}/?format=json&include=[LinkedTestPlan[TestCases[Id]]]&access_token={ACCESS_TOKEN}")
+        return response.json()
--- tests/test_updater.py
+++ tests/test_updater.py
@@ -5,21 +5,48 @@ import requests
 class FeatureCheckSuccessResponse:
     status_code = 200
 
+    @staticmethod
+    def json():
+        return {"Items": [{"ResourceType": "Feature","Id": 99999}]}
+
 class FeatureCheckFailureResponse:
     status_code = 404
 
+class GetFeatureTestCases:
+    
+    @staticmethod
+    def json():
+        return {"ResourceType": "Feature","Id": 99999,"LinkedTestPlan": {
+            "ResourceType": "TestPlan",
+            "Id": 99998,
+            "TestCases": {
+                "Items": [
+                    {"ResourceType": "TestCase","Id": 34565},
+                    {"ResourceType": "TestCase","Id": 34566},
+                    {"ResourceType": "TestCase","Id": 34567}]}}}
+
+
 @fixture
 def feature_updater():
-    return Updater("Feature: Whole Feature", 99999, 12345)
+    return Updater("Feature: Whole Feature", 12345)
+
 
 @fixture
 def non_existent_feature_updater():
-    return Updater("Feature: Not A Whole Feature", 88888, 12345)
+    return Updater("Feature: Not A Whole Feature", 12345)
 
+@fixture
+def known_feature_to_update():
+    updater = Updater("Feature: Whole Feature", 12345)
+    updater.feature_id = 99999
+    return updater
 
-def test_that_updater_intialises_with_feature_id(feature_updater):
-    assert feature_updater.feature_id == 99999
-
+@fixture
+def features_test_cases_are_known():
+    updater = Updater("Feature: Whole Feature", 12345)
+    updater.feature_id = 99999
+    updater.test_cases = [34565, 34566, 34567]
+    return updater
 
 def test_updater_creates_a_request_to_check_that_a_feature_exists(feature_updater):
     assert feature_updater.check_feature_exists_request_url == "https://bluefruit.tpondemand.com/api/v1/Features?where=(Name eq 'Feature: Whole Feature') and (Project.Id eq 12345)&include=[Id]&format=json&access_token=testaccesstoken"
@@ -42,3 +69,46 @@ def test_updater_does_not_find_a_feature(non_existent_feature_updater, monkeypat
     assert(non_existent_feature_updater.check_feature() == False)
 
 
+def test_updater_sets_feature_id_if_a_feature_exists(feature_updater, monkeypatch):
+    def mock_get(*args, **kwargs):
+        return FeatureCheckSuccessResponse()
+
+    monkeypatch.setattr(requests, "get", mock_get)
+    feature_updater.check_feature()
+    assert feature_updater.feature_id == 99999
+    
+
+def test_updater_gets_a_list_of_test_cases_linked_to_a_known_feature(known_feature_to_update, monkeypatch):
+    def mock_get(*args, **kwargs):
+        return GetFeatureTestCases()
+
+    expected_response = {"ResourceType": "Feature","Id": 99999,"LinkedTestPlan": {
+            "ResourceType": "TestPlan",
+            "Id": 99998,
+            "TestCases": {
+                "Items": [
+                    {"ResourceType": "TestCase","Id": 34565},
+                    {"ResourceType": "TestCase","Id": 34566},
+                    {"ResourceType": "TestCase","Id": 34567}]}}}
+
+    monkeypatch.setattr(requests, "get", mock_get)
+    assert known_feature_to_update.get_test_cases_of_an_existing_feature() == expected_response
+
+
+def test_updater_stores_list_of_all_test_case_ids_of_a_known_feature(known_feature_to_update, monkeypatch):
+    def mock_get_test_cases_of_an_existing_feature(*args, **kwargs):
+        return {"ResourceType": "Feature","Id": 99999,"LinkedTestPlan": {
+            "ResourceType": "TestPlan",
+            "Id": 99998,
+            "TestCases": {
+                "Items": [
+                    {"ResourceType": "TestCase","Id": 34565},
+                    {"ResourceType": "TestCase","Id": 34566},
+                    {"ResourceType": "TestCase","Id": 34567}]}}}
+        
+    monkeypatch.setattr(Updater, "get_test_cases_of_an_existing_feature", mock_get_test_cases_of_an_existing_feature)
+    known_feature_to_update.set_test_cases_of_an_existing_feature()
+    assert known_feature_to_update.test_cases == [34565, 34566, 34567]
+
+def test_updater_splits_tests_out_that_dont_exist_in_feature(features_test_cases_are_known):
+    
